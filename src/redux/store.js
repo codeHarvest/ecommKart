b6976cb52c9affd8b52653751be898a1aa97c4d7
codeHarvest <madhusudanbhardwaj94@gmail.com>
2019-11-10 22:56:08 +0530

    persisting cart items with redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import combineReducers from '../redux/root-reducer';
@@ -6,6 +7,8 @@ import combineReducers from '../redux/root-reducer';
 
 const middleWares = [logger];
 
-const store = createStore(combineReducers, applyMiddleware(...middleWares));
+export const store = createStore(combineReducers, applyMiddleware(...middleWares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store , persistor};
